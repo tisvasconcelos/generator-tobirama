ef432d425428a3b82da537ca8d43ed8b5f129c9f
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2013-08-21 16:25:02 -0300

    Documentation fix

--- app/templates/code-igniter/templates/default/welcome_message.php
+++ app/templates/code-igniter/templates/default/welcome_message.php
@@ -14,7 +14,7 @@
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>templates/default/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/welcome.php</code>
